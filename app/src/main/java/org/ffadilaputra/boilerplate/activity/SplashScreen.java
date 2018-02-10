438237ba463eab7deed79487d2ba44d728fa969f
Ivan Fadila Putra <i.fadilaputra@gmail.com>
2018-02-10 14:52:49 +0700

    Init

---
+++ app/src/main/java/org/ffadilaputra/boilerplate/activity/SplashScreen.java
@@ -0,0 +1,44 @@
+package org.ffadilaputra.boilerplate.activity;
+
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.flaviofaria.kenburnsview.RandomTransitionGenerator;
+
+import org.ffadilaputra.boilerplate.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 5000;
+    private KenBurnsView mKenBurns;
+    private long duration = 2500;
+    private TextView welcomeText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        mKenBurns = (KenBurnsView) findViewById(R.id.image);
+        mKenBurns.setImageResource(R.drawable.splash);
+        welcomeText = (TextView) findViewById(R.id.welcome_text);
+  		ObjectAnimator alphaAnimation = ObjectAnimator.ofFloat(welcomeText, "alpha", 0.0F, 1.0F);
+		alphaAnimation.setStartDelay(1700);
+		alphaAnimation.setDuration(500);
+		alphaAnimation.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+
+}
